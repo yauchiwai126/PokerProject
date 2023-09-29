a6763e40ca73fd7f1f0718ddfb1e897b62a1aa58
yauchiwai126 <yauchiwai@hotmail.com>
2023-09-29 19:59:05 +0800

    3rd Commit

--- PokerProject/src/Poker/Poker.java
+++ PokerProject/src/Poker/Poker.java
@@ -33,7 +33,7 @@ public class Poker {
 	}
 	
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Hello World3");
 		System.out.println(new Poker().isFullHouse(
 			new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
 	}
